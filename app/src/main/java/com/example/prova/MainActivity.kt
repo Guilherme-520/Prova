ed32a67e703425a69d1011dda3f7589a1e9adf57
Guilherme-520 <guilhermecordeiro520@outlook.com>
2022-09-23 18:10:46 -0300

    conectado com firebase

--- app/src/main/java/com/example/prova/MainActivity.kt
+++ app/src/main/java/com/example/prova/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.prova
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,10 +20,29 @@ class MainActivity : AppCompatActivity() {
         val db = Firebase.firestore
 
         val btnCad: Button = findViewById(R.id.btnCad)
-        val Bairro: EditText = findViewById(R.id.bairro)
+        val bairro: EditText = findViewById(R.id.bairro)
         val endereco: EditText = findViewById(R.id.endereco)
         val cep: EditText = findViewById(R.id.cep)
         val nome: EditText = findViewById(R.id.nome)
+
+
+        // Create a new user with a first and last name
+        val pessoa = hashMapOf(
+            "nome" to nome.text.toString(),
+            "bairro" to bairro.text.toString(),
+            "endereco" to endereco.text.toString(),
+            "cep" to endereco.text.toString()
+        )
+
+// Add a new document with a generated ID
+        db.collection("cadastro")
+            .add(pessoa)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
 
     }
 }
